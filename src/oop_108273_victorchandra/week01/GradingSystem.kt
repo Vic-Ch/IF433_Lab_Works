84f1a3fab45ea9caa0bf5a472bc4d3435326f2a7
Vic-Ch <vchan.study@gmail.com>
2026-02-06 22:11:26 +0700

    week01: initial setup and procedural code

---
+++ src/oop_108273_victorchandra/week01/GradingSystem.kt
@@ -0,0 +1,9 @@
+package oop_108273_victorchandra.week01
+
+fun main() {
+  var name: String = "John Thor"
+  var score: Int = 80
+
+  // Concat gaya lama
+  println("Nama " + name + ", Nilai: " + score)
+}
