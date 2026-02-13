86f56e4173b418322366402e615ce77c5f761528
Vic-Ch <vchan.study@gmail.com>
2026-02-13 19:55:55 +0700

    week02: implement selection logic for constructors

--- src/oop_108273_victorchandra/week02/Main.kt
+++ src/oop_108273_victorchandra/week02/Main.kt
@@ -15,15 +15,26 @@ fun main() {
 
   scanner.nextLine()
 
-  // validasi input
+  // validasi input (izin modifikasi dari yand di modul agar lebih bersih untuk jalur reguler dan umum
   if (nim.length != 5) {
     println("ERROR: Pendaftaran dibatalkan. NIM haris 5 karakter!")
   } else {
-    print("Masukkan jurusan: ")
-    val major = scanner.nextLine()
+    print("Pilih Jalur (1. Reguler, 2. Umum): ")
+    val type = scanner.nextInt()
+    scanner.nextLine()
 
-    // instance objek
-    val s1 = Student(name, nim, major)
-    println("Status: Pendaftaran selesai.")
+    if (type == 1) {
+      print("Masukkan jurusan: ")
+      val major = scanner.nextLine()
+      val s1 = Student(name, nim, major)
+      println("Status: Pendaftaran selesai.")
+      println("Terdaftar di: ${s1.major} dengan GPA awal ${s1.gpa}")
+    } else if (type == 2) {
+      val s2 = Student(name, nim)
+      println("Status: Pendaftaran selesai.")
+      println("Terdaftar di: ${s2.major} dengan GPA awal ${s2.gpa}")
+    } else {
+      println("Pilihan ngawur, pendaftaran batal!")
+    }
   }
 }
