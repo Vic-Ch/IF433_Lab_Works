a9ad831f0f728a63fe4305f8a7120b06481f91f5
Vic-Ch <vchan.study@gmail.com>
2026-02-20 12:08:01 +0700

    week03: cleanup code

--- src/oop_108273_victorchandra/week03/Main.kt
+++ src/oop_108273_victorchandra/week03/Main.kt
@@ -2,16 +2,11 @@ package oop_108273_victorchandra.week03
 
 fun main()  {
   val e = Employee("Budi")
-
-  // test validasi salary
   e.salary = -1000
   e.salary = 5000000
   println("Gaji: ${e.salary}")
 
-  // test enkapsulasi
   e.increasePerfomance()
-//  e.perfomanceRating = 5 // test yang private
 
-  // test computed property
   println("Pajak yang harus dibayar: ${e.tax}")
 }
