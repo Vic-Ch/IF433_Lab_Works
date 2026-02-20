7d6988de862490fb7027e9e611320991714f6958
Vic-Ch <vchan.study@gmail.com>
2026-02-20 12:29:58 +0700

    week03: (task 1) implement Weapon testing in main

--- src/oop_108273_victorchandra/week03/Main.kt
+++ src/oop_108273_victorchandra/week03/Main.kt
@@ -1,5 +1,7 @@
 package oop_108273_victorchandra.week03
 
+import java.util.Scanner
+
 fun main()  {
   val e = Employee("Budi")
   e.salary = -1000
@@ -9,4 +11,19 @@ fun main()  {
   e.increasePerfomance()
 
   println("Pajak yang harus dibayar: ${e.tax}")
+
+  println("--- Task 1 ---")
+  val scanner = Scanner(System.`in`)
+
+  print("Masukkan nama senjata: ")
+  val name = scanner.nextLine()
+
+  print("Masukkan damage senjata: ")
+  val dmg = scanner.nextInt()
+  scanner.nextLine()
+
+  val w1 = Weapon(name)
+
+  w1.damage = dmg
+  println("Senjata ${w1.name} dengan damage ${w1.damage}, tiernya: ${w1.tier}")
 }
