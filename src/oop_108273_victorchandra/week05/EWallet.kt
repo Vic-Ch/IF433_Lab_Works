f208fe72eca112bb672ab371ce696aba94d40915
Vic-Ch <vchan.study@gmail.com>
2026-03-07 00:23:18 +0700

    week05: (task 2) create CreditCard class

--- src/oop_108273_victorchandra/week05/EWallet.kt
+++ src/oop_108273_victorchandra/week05/EWallet.kt
@@ -6,11 +6,14 @@ class EWallet(accountName: String): PaymentMethod(accountName) {
   override fun processPayment(amount: Double) {
     if(balance >= amount) {
       balance -= amount
-      println("Sukses")
+      println("Transaksi sukses")
+    } else {
+      println("Saldo tidak cukup")
     }
   }
 
   fun topUp(amount: Double) {
     balance += amount
+    println("Berhasil top-up sebesar $amount. Saldo sekarang $balance")
   }
 }
