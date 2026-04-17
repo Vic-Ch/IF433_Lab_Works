9433a12c8cfb0fd59d9877f10b09e1efed702628
Vic-Ch <vchan.study@gmail.com>
2026-04-18 00:47:44 +0700

    week08: (task) implement payment processing with !!

--- src/oop_108273_victorchandra/week08/ApiParser.kt
+++ src/oop_108273_victorchandra/week08/ApiParser.kt
@@ -1,23 +1,38 @@
 package oop_108273_victorchandra.week08
 
-fun parseProduct(rawJson: Map<String, Any?>): Product? {
-  val id = requireNotNull(rawJson["id"]) {
-    "API Invalid: Missing ID"
+class ApiParser {
+  fun parseProduct(rawJson: Map<String, Any?>): Product? {
+    val id = requireNotNull(rawJson["id"]) {
+      "API Invalid: Missing ID"
+    }
+
+    val name = requireNotNull(rawJson["name"]) {
+      "API Invalid: Missing Name"
+    }
+
+    val type: String = rawJson["type"].toString()
+
+    if (type.uppercase() == "ELECTRONIC") {
+      val warranty = rawJson["warranty"] as? Int ?: 12
+      return Product.Electronic(id.toString(), name.toString(), warranty)
+    } else if (type.uppercase() == "CLOTHING") {
+      val size = rawJson["size"] as? String ?: "All Size"
+      return Product.Clothing(id.toString(), name.toString(), size)
+    } else {
+      return null
+    }
   }
 
-  val name = requireNotNull(rawJson["name"]) {
-    "API Invalid: Missing Name"
-  }
-
-  val type: String = rawJson["type"].toString()
-
-  if (type.uppercase() == "ELECTRONIC") {
-    val warranty = rawJson["warranty"] as? Int ?: 12
-    return Product.Electronic(id.toString(), name.toString(), warranty)
-  } else if (type.uppercase() == "CLOTHING") {
-    val size = rawJson["size"] as? String ?: "All Size"
-    return Product.Clothing(id.toString(), name.toString(), size)
-  } else {
-    return null
+  fun checkOut(product: Product) {
+    when(product) {
+      is Product.Clothing -> {
+        val clot = JavaPaymentService.processPayment(product.id)
+        println(clot!!)
+      }
+      is Product.Electronic -> {
+        val elec = JavaPaymentService.processPayment(product.id)
+        println(elec!!)
+      }
+    }
   }
 }
