b7b9b74510b7cccb58119a1755e5f4ccf0487808
Vic-Ch <vchan.study@gmail.com>
2026-05-08 11:54:04 +0700

    week11: (task) configure Philips WiZ using apply

--- src/oop_108273_victorchandra/week11/SmartHomeDashboard.kt
+++ src/oop_108273_victorchandra/week11/SmartHomeDashboard.kt
@@ -2,4 +2,17 @@ package oop_108273_victorchandra.week11
 
 fun main() {
   val homeDevices = mutableListOf<SmartDevice>()
+
+  val device = SmartDevice().apply {
+    name = "Phillips WiZ Living Room"
+    category = "Lighting"
+    isOnline = true
+    powerLoad = 12
+  }
+
+  homeDevices.also {
+    println("Sebelum: $it")
+  }.add(device)
+
+  println("Setelah: $homeDevices")
 }
--- src/oop_108273_victorchandra/week11/SmartHomeModels.kt
+++ src/oop_108273_victorchandra/week11/SmartHomeModels.kt
@@ -1,6 +1,6 @@
 package oop_108273_victorchandra.week11
 
-data class SmartDevice(var name: String, var category: String,
+data class SmartDevice(var name: String = "", var category: String = "",
   var isOnline: Boolean = false, var powerLoad: Int = 0)
 
 fun SmartDevice.diagnose(): String {
