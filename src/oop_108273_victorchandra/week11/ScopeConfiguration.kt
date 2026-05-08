9000e2cbb820d7eab41efa3b0f487caa15b07004
Vic-Ch <vchan.study@gmail.com>
2026-05-08 11:24:00 +0700

    week11: implement also for logging side effects

--- src/oop_108273_victorchandra/week11/ScopeConfiguration.kt
+++ src/oop_108273_victorchandra/week11/ScopeConfiguration.kt
@@ -11,4 +11,12 @@ fun main() {
   }
 
   println(user)
+
+  println("\n=== TEST ALSO ===")
+  val numbers = mutableListOf(1,2,3)
+  numbers.also {
+    println("Log sebelum ditambah: $it")
+  }.add(4)
+
+  println("Setelah ditambah: $numbers")
 }
