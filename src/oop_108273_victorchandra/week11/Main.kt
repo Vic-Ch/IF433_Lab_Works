53808cbde1e854d65fbda33a9c04c79798f86f08
Vic-Ch <vchan.study@gmail.com>
2026-05-08 10:54:30 +0700

    week11: test extension functions in main

---
+++ src/oop_108273_victorchandra/week11/Main.kt
@@ -0,0 +1,10 @@
+package oop_108273_victorchandra.week11
+
+fun main() {
+  println("=== TEST EXTENSION FUNCTIONS ===")
+  println("Alex".addGreeting())
+  println("Hi".repeatTimes(3))
+
+  val text: String? = "Ayam"
+  println("Apakah null/empty? ${text.isNullOrEmptyCustom()}")
+}
