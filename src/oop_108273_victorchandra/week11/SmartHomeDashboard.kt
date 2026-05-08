dea71f84e34cd1c27b6d6ce520dfdc65696fe0e2
Vic-Ch <vchan.study@gmail.com>
2026-05-08 12:05:55 +0700

    week11: (task) configure AC unit using run

--- src/oop_108273_victorchandra/week11/SmartHomeDashboard.kt
+++ src/oop_108273_victorchandra/week11/SmartHomeDashboard.kt
@@ -27,4 +27,10 @@ fun main() {
 
   homeDevices.add(device2)
 
+
+  homeDevices.run {
+    this.add(SmartDevice("Daikin Inverter (Kabel 3x2.5)", "HVAC", false, 800))
+    this.add(SmartDevice("Picolo's Auto Feeder", "Pet Care", true, 10))
+    println("\n$this")
+  }
 }
