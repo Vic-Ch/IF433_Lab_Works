4c34d959073148184adcc0fd11cd1018c1ef1203
Vic-Ch <vchan.study@gmail.com>
2026-05-08 12:01:05 +0700

    week11: (task) configure Ezviz camera with also

--- src/oop_108273_victorchandra/week11/SmartHomeDashboard.kt
+++ src/oop_108273_victorchandra/week11/SmartHomeDashboard.kt
@@ -15,4 +15,16 @@ fun main() {
   }.add(device)
 
   println("Setelah: $homeDevices")
+
+  val device2 = SmartDevice("Ezviz Outdoor", "Camera")
+  device2.apply {
+    isOnline = true
+    powerLoad = 5
+  }.also {
+    println("\n(LOG) Kamera terhubung")
+    println("-> ${it.diagnose()}")
+  }
+
+  homeDevices.add(device2)
+
 }
