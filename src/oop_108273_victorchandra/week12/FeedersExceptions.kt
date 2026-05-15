211a0ad47afb4aaa39958966640f9aa75998212d
Vic-Ch <vchan.study@gmail.com>
2026-05-15 11:24:09 +0700

    week12: (task) handle feeding execution with multiple catch

--- src/oop_108273_victorchandra/week12/FeedersExceptions.kt
+++ src/oop_108273_victorchandra/week12/FeedersExceptions.kt
@@ -1,7 +1,6 @@
 package oop_108273_victorchandra.week12
 
-sealed class FeedersExceptions(msg: String): Exception(msg)
-
-class FoodEmptyException(requested: Int, available: Int): FeedersExceptions("Kibble tidak cukup! Diminta $requested gr, sisa $available gr")
-
-class DispenserJamException: FeedersExceptions("Wadah dispender tersangkut/macet!")
+sealed class FeedersExceptions(msg: String): Exception(msg) {
+  class FoodEmptyException(requested: Int, available: Int): FeedersExceptions("Kibble tidak cukup! Diminta $requested gr, sisa $available gr")
+  class DispenserJamException: FeedersExceptions("Wadah dispender tersangkut/macet!")
+}
--- src/oop_108273_victorchandra/week12/SmartFeeder.kt
+++ src/oop_108273_victorchandra/week12/SmartFeeder.kt
@@ -1,13 +1,26 @@
 package oop_108273_victorchandra.week12
 
-fun dispenceKibble(requestedGram: Int, availableGram: Int, isJammed: Boolean): Int {
+fun dispenseKibble(requestedGram: Int, availableGram: Int, isJammed: Boolean): Int {
   require(requestedGram > 0) { "Porsi kibble harus lebih dari 0 gr" }
-  var x = availableGram
 
-  if (x < requestedGram) throw FoodEmptyException(availableGram, requestedGram)
-  if (isJammed) throw DispenserJamException()
+  if (isJammed) throw FeedersExceptions.DispenserJamException()
+  if (availableGram < requestedGram) throw FeedersExceptions.FoodEmptyException(requestedGram, availableGram)
 
-  x -= requestedGram
   println("Kibble berhasil dikeluarkan!")
-  return x
+
+  return availableGram - requestedGram
+}
+
+fun main() {
+  val currentKibbleStock = 10
+  try {
+    dispenseKibble(80, currentKibbleStock, false)
+  } catch (e: FeedersExceptions.DispenserJamException) {
+    println("Dispenser Error: ${e.message}")
+  } catch (e: FeedersExceptions.FoodEmptyException) {
+    println("Food Empty Error: ${e.message}")
+  } catch (e: Exception) {
+    println("General Error: ${e.message}")
+  }
+
 }
