408a4b5b5c64b4f7360280f8adb6175c9b79cef7
Vic-Ch <vchan.study@gmail.com>
2026-05-15 11:03:56 +0700

    week12: (task) implement dispenseKibble function

--- src/oop_108273_victorchandra/week12/SmartFeeder.kt
+++ src/oop_108273_victorchandra/week12/SmartFeeder.kt
@@ -2,11 +2,12 @@ package oop_108273_victorchandra.week12
 
 fun dispenceKibble(requestedGram: Int, availableGram: Int, isJammed: Boolean): Int {
   require(requestedGram > 0) { "Porsi kibble harus lebih dari 0 gr" }
-//  if (isJammed) throw DispenserJamException()
   var x = availableGram
 
   if (x < requestedGram) throw FoodEmptyException(availableGram, requestedGram)
+  if (isJammed) throw DispenserJamException()
 
   x -= requestedGram
+  println("Kibble berhasil dikeluarkan!")
   return x
 }
